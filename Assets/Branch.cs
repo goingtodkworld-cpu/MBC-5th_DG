059f308bf0e0dcc7baa5dc4e7add2c81e1171d01
goingtodkworld-cpu <goingtodkworld@gmail.com>
2025-10-23 12:01:36 +0900

    어렵다잉

--- Assets/Branch.cs
+++ Assets/Branch.cs
@@ -7,7 +7,7 @@ public class Branch : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("¾î·Æ´ÙÀ×");
     }
 
     // Update is called once per frame
